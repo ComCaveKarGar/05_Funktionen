6c63e6f6621c97167b3b478fb13d5d74fc1a6340
Karsten <Moonjumper@gmx.de>
2023-05-12 08:03:09 +0200

    letzte Änderungen

--- script_01.js
+++ script_01.js
@@ -46,12 +46,53 @@
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter
 
-ausgabeNamenParams( "Dave", "Batista");
-ausgabeNamenParams("Fritz", "Kalkbrenner");
-ausgabeNamenParams(prompt("Wie ist dein Vorname?"), prompt("Wie ist dein Nachname?"))
+// ausgabeNamenParams( "Dave", "Batista");
+// ausgabeNamenParams("Fritz", "Kalkbrenner");
+// ausgabeNamenParams(prompt("Wie ist dein Vorname?"), prompt("Wie ist dein Nachname?"))
 
-function ausgabeNamenParams(firstName, familyName) 
+// function ausgabeNamenParams(firstName, familyName) 
 
-{
-    console.log("Moin " + firstName + " " + familyName + "!");
+// {
+//     console.log("Moin " + firstName + " " + familyName + "!");
+// }
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob) --> Begründer: Cecil Martin aka Uncle Bob (Nice to Know)
+// SRP single responsibility principle
+
+// ausgabeNamenParams2("Jamshid", "Rahmanzani");
+
+// function ausgabeNamenParams2(firstName, familyName) {
+
+//      // 1. Funktionalität: string composing
+//     const GAP = " ";
+//     let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    
+//      // 2. Funktionalität: string output
+//     console.log(outputStr);
+// }
+
+/***** Funktionen 03b *****/
+// Trennen der Funktionalitäten in Funktionen | return
+
+// getString("Maxine","Mützerich");
+// output(getString("Maxine","Mützerich"));
+output(getString(prompt("Vorname?"),prompt("Nachname?")));
+
+function getString(firstName,familyName) {
+    const GAP = " ";
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    return outputStr; // return sendet Daten an den call zurück
+    console.log(outputStr); // return bricht die Funktion ab!
+}
+
+/** INPUT --> Parameter | return --> OUTPUT **/
+
+// 2. Funktionalität: string output
+// output("Hi");
+// output(2);
+// output(true);
+function output(outputData) {    // Das hier nennt man Wrapper! Wird uns später immer wieder begegnen und den Arsch retten!
+    console.log(outputData);
 }
